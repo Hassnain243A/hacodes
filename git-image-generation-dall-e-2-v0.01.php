d1cfa5fdabd92eb3c204840c87e7d5fc35530a14
Hassnain Amjad <91315215+Hassnain243A@users.noreply.github.com>
2023-01-14 17:29:19 +0500

    Update git-image-generation-dall-e-2-v0.01.php

--- git-image-generation-dall-e-2-v0.01.php
+++ git-image-generation-dall-e-2-v0.01.php
@@ -3,11 +3,15 @@ require __DIR__ . '/vendor/autoload.php';
 use Orhanerday\OpenAi\OpenAi;
 $open_ai = new OpenAi('your-api-key');
 $prompt = $_POST['prompt'];
-$complete = $open_ai->image([
+// prepare object for request
+[
     "prompt" => $prompt,
     "n" => 3,
     "size" => "256x256",
     "response_format" => "b64_json",
-]);
+]
+$complete = $open_ai->image(
+// object goes here
+);
 echo $complete;
 ?>
