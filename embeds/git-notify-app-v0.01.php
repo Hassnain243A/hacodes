cfeac8dffc615a6aaf7c627a19e5c9b56716b68a
Hassnain Amjad <91315215+Hassnain243A@users.noreply.github.com>
2023-07-22 22:14:06 +0500

    Add files via upload

---
+++ embeds/git-notify-app-v0.01.php
@@ -0,0 +1,51 @@
+<?php
+$client->setAuthConfig("sample-6140e-firebase******.json");
+
+include "./get_access_token.php";
+$access_token = get_access_token();
+
+$device_tokens = [
+    "****",
+    "****",
+    "****"
+];
+
+foreach ($device_tokens as $token) {
+    $response = sendFCMNotification($access_token, $token);
+    echo $response . '<br>';
+}
+
+function sendFCMNotification($access_token, $token) {
+    $url = "https://fcm.googleapis.com/v1/projects/your-project-id-here/messages:send";
+    ...
+}
+
+$data = [
+    'message' => [
+        "data"=> [
+            "title" => "Title",
+            "body" => "This is message body.",
+            "icon" => "https://www.clipscutter.com/image/brand/brand-256.png",
+            "image" => "https://images.unsplash.com/photo-1514473776127-61e2dc1dded3?w=871&q=80",
+            "click_action" => "https://example.com"
+        ],
+        'token' => $token
+    ]
+];
+
+$options = array(
+    CURLOPT_URL => $url,
+    CURLOPT_POST => true,
+    CURLOPT_HTTPHEADER => array(
+        "Authorization: Bearer " . $access_token,
+        "Content-Type: application/json",
+    ),
+    CURLOPT_POSTFIELDS => json_encode($data),
+);
+
+$curl = curl_init();
+curl_setopt_array($curl, $options);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($curl);
+curl_close($curl);
+return $response;
